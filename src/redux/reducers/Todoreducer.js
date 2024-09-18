026eca1d8ed5786916ea7d8a79cec051676421d5
satyapriya panigrahy <satyapriyapanigrahy2002@gmail.com>
2024-09-18 16:38:57 +0530

    React Project

---
+++ src/redux/reducers/Todoreducer.js
@@ -0,0 +1,13 @@
+ 
+ import {createreducer} from "react-redux"
+
+ const todo=createreducer({
+    name:todo,
+    initalState:{},
+        reducers:{
+          addtodo:(type,action)=>
+
+          store.storeReducer
+        }
+
+ })
---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import {legacy_createStore} from "redux"
+
+const store=legacy_createStore({
+
+reducer:{
+    todos:todoreducer
+},
+})
+
+export default store
